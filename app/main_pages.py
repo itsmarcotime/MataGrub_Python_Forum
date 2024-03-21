56382b67ea4637a00ef6bea4a35040ade8484f86
Marco <patrickstar1281@yahoo.com>
2024-03-20 22:28:45 -0500

    added base temp and styles

--- app/main_pages.py
+++ app/main_pages.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 main_pages = Blueprint("main_pages", __name__)
 
-@main_pages.route('/home')
-def home():
-    return render_template('test.html')
+@main_pages.route('/')
+def base_page():
+    return render_template('base.html', the_title='Welcome to the Mata Forum Page!')
