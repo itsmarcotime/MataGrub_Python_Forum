4770ad28d727e937b628a9cb96324ecdf5fd12f1
Marco <patrickstar1281@yahoo.com>
2024-03-21 22:14:20 -0500

    setting up the hime page

--- app/main_pages.py
+++ app/main_pages.py
@@ -5,3 +5,15 @@ main_pages = Blueprint("main_pages", __name__)
 @main_pages.route('/')
 def base_page():
     return render_template('base.html', the_title='Welcome to the Mata Forum Page!')
+
+@main_pages.route('/home')
+# @check_logged_in
+def home():
+    cursor.execute("SELECT * FROM articles")
+    posts = cursor.fetchall()
+
+    if posts:
+        return render_template('home.html', username=session['username'], articles=posts)
+    else:
+        msg = 'No posts have been found. Click on create post!'
+        return render_template('home.html', username=session['username'], msg=msg)
